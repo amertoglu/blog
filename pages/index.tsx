985846bef279bfcefea25f9992464434bd191abe
A. Mertoğlu <155421193+amertoglu@users.noreply.github.com>
2024-03-11 02:54:37 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Container from "../components/container";
-import Image from "next/image";
 
 function HomePage() {
   return (
@@ -7,7 +6,7 @@ function HomePage() {
       <Container>
         <div className="space-y-6">
           <h1 className="text-2xl font-bold">
-            Hello there! 👋
+            Hi, there! 👋
           </h1>
           <p>
             I'm A Mertoğlu, a passionate web developer with a love for creativity and innovation. Welcome to my corner of the internet, where I bring ideas to life through code.
@@ -16,15 +15,6 @@ function HomePage() {
           <p>Thanks for stopping by, and I hope you enjoy exploring my website!</p>
         </div>
       </Container>
-
-      <div className="container max-w-4xl m-auto px-4 mt-20">
-        <Image
-          src="/"
-          alt="avatar"
-          width="80"
-          height="80"
-        />
-      </div>
     </>
   );
 }
