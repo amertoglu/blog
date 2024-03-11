c781c86b80934c1230006d9a7a03d01fffa86afc
A. Mertoğlu <155421193+amertoglu@users.noreply.github.com>
2024-03-11 03:17:51 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,10 +12,10 @@ function HomePage() {
             I'm A Mertoğlu, a passionate web developer with a love for creativity and innovation. Welcome to my corner of the internet, where I bring ideas to life through code. 👨‍💻
           </p>
           <p>
-            I specialize in crafting beautiful and functional websites that not only look great but also provide seamless user experiences. With expertise in HTML, CSS, JavaScript, and various web development frameworks, I bring ideas to life in the digital world.🔥
+            I specialize in crafting beautiful and functional websites that not only look great but also provide seamless user experiences. With expertise in HTML, CSS, JavaScript, and various web development frameworks, I bring ideas to life in the digital world. 🔥
           </p>
 
-          <p>Thanks for visiting my website.✨</p>
+          <p>Thanks for visiting my website. ✨</p>
         </div>
       </Container>
     </>
