822aa1ee19612dac515ba1cb430190c1c3cc789b
A. Mertoğlu <155421193+amertoglu@users.noreply.github.com>
2024-03-11 03:27:35 +0300

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -6,10 +6,10 @@ export default function Header() {
     <header className="py-6">
       <Container>
         <nav className="flex space-x-4">
-          <Link href="/">/.</Link>
-          <Link href="/posts">Posts.</Link>
-          <Link target="_blank" href="https://github.com/amertoglu">Works.</Link>
-          <Link target="_blank" href="https://twitter.com/web0dev">Twitter.</Link>
+          <Link href="/">/</Link>
+          <Link href="/posts">Posts</Link>
+          <Link target="_blank" href="https://github.com/amertoglu">Works</Link>
+          <Link target="_blank" href="https://twitter.com/web0dev">Twitter</Link>
         </nav>
       </Container>
     </header>
