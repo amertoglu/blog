78e82e8f5fa35dbc0a0b5f3314e2031eb1ca022c
A. Mertoğlu <155421193+amertoglu@users.noreply.github.com>
2024-03-14 20:16:36 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,11 @@ function HomePage() {
       <Container>
         <div className="space-y-6">
           <h1 className="text-2xl font-bold">
-            I'm A Mertoğlu, a passionate web developer with a love for creativity and innovation. Welcome to my corner of the internet, where I bring ideas to life through code.
+            I'm A Mertoğlu, a passionate web developer with a love for creativity and innovation. 
           </h1>
+          <p>
+            Welcome to my corner of the internet, where I bring ideas to life through code.
+          </p>
           <p>
             I specialize in crafting beautiful and functional websites that not only look great but also provide seamless user experiences. With expertise in HTML, CSS, JavaScript, and various web development frameworks, I bring ideas to life in the digital world.
           </p>
